96125c1ddc63abc7f27e9cb3ae414be6c4d042a4
Patrick Dziura <patrickdziura@gmail.com>
2019-11-22 00:34:47 -0300

    Update server.R
    Testando heatmap

--- ShinyBD/server.R
+++ ShinyBD/server.R
@@ -1,13 +1,21 @@
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-  
-  output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'blue')
-  })
-}
+library(pheatmap)
 
+function(input, output, session) {
+  
+  d1=read.table("student/student-mat.csv",sep=";",header=TRUE)
+  d2=read.table("student/student-por.csv",sep=";",header=TRUE)
+  d3=merge(d1,d2,by=c("school","sex","age","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
+  
+  # Combine the selected variables into a new data frame
+output$plot1 <- renderPlot({
+  ind <- NULL
+  for (i in 1:ncol(d3)) {
+    ind <- c(ind, is.numeric(d3[,i]))
+  }
+  num_d3 <- d3[,ind]
+  num_d3 <- as.matrix(num_d3)
+  cor_d3 <- cor(num_d3)
+  pheatmap(cor_d3)
+  })
+  
+}
